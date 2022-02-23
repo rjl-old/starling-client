2429a4e1a5c1377ffbc3d28da059bdbd714a314c
Richard <richlyon@mac.com>
2022-02-23 11:23:09 +0000

    add component AccountList

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { SidebarNavigation } from "../components/SidebarNavigation";
+import TransactionList from "../components/AccountList";
 
 export default function Home() {
   return (
@@ -9,7 +10,18 @@ export default function Home() {
       </div>
 
       {/* Main content */}
-      <div className="md:pl-64 flex flex-col flex-1">Main content</div>
+      <div className="md:pl-64 flex flex-col flex-1">
+        <main className={"flex-1"}>
+          <div className={"px-4 py-6"}>
+            {/* Main content title */}
+            <h1 className={" text-2xl font-semibold"}>Home</h1>
+            {/* Content */}
+            <div>
+              <TransactionList />
+            </div>
+          </div>
+        </main>
+      </div>
     </>
   );
 }
