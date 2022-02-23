18846970131281fd7f50a881ce52bf5aa054fd9d
Richard <richlyon@mac.com>
2022-02-23 09:19:34 +0000

    index wip

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,62 @@
+import {
+  CogIcon,
+  HomeIcon,
+  SwitchHorizontalIcon,
+} from "@heroicons/react/outline";
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+const navigation = [
+  { name: "Home", href: "#", icon: HomeIcon, current: true },
+  { name: "Payments", href: "#", icon: SwitchHorizontalIcon, current: false },
+  { name: "Account Details", href: "#", icon: CogIcon, current: false },
+];
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      {/* Static sidebar for desktop */}
+      <div className="md:flex md:w-64 md:flex-col md:fixed md:inset-y-0">
+        {/* Sidebar component, swap this element with another sidebar if you like */}
+        <div className="flex-1 flex flex-col min-h-0 border-r border-gray-200 bg-white">
+          <div className="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
+            {/* Logo */}
+            <div className="flex items-center flex-shrink-0 px-4">
+              <p className={"text-xl font-bold"}>Starling client</p>
+            </div>
+            {/* Navigation */}
+            <nav className={"flex-1 mt-5 px-2 bg-white"}>
+              {navigation.map((item) => (
+                <a
+                  key={item.name}
+                  href={item.href}
+                  className={classNames(
+                    item.current
+                      ? "bg-gray-100 text-gray-900"
+                      : "text-gray-600 hover:bg-gray-50 hover:text-gray-900",
+                    "group flex items-center px-2 py-2 text-sm font-medium rounded-md"
+                  )}
+                >
+                  <item.icon
+                    className={classNames(
+                      item.current
+                        ? "text-gray-500"
+                        : "text-gray-400 group-hover:text-gray-500",
+                      "mr-3 flex-shrink-0 h-6 w-6"
+                    )}
+                  />
+                  {item.name}
+                </a>
+              ))}
+            </nav>
+          </div>
+        </div>
+      </div>
+
+      {/* Main content */}
+      <div className="md:pl-64 flex flex-col flex-1">Main content</div>
+    </>
+  );
 }
