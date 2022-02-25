196285d3318e179f15484e17ea71b08c47f3153b
Richard <richlyon@mac.com>
2022-02-25 10:22:36 +0000

    prep for account details feature

--- pages/account_details.tsx
+++ pages/account_details.tsx
@@ -1,14 +1,34 @@
 import { Layout } from "../components/Layout";
-import { SidebarNavigation } from "../components/SidebarNavigation";
+import { useGetAccountsAccountsGet } from "../api/service/accounts";
 
 export default function AccountDetails() {
+  const response = useGetAccountsAccountsGet();
+  const mainAccounts = response.data?.main_accounts;
   return (
     <>
       {/* Main content title */}
-      <h1 className={" text-2xl font-semibold pb-4"}>Account Details</h1>
+      <h1 className={" text-2xl font-semibold pb-4"}>Accounts</h1>
 
       {/* Content */}
-      <div></div>
+      <h2 className="text-gray-500 text-xs font-medium uppercase tracking-wide">
+        Accounts:
+      </h2>
+      <ul
+        role="list"
+        className="mt-3 grid grid-cols-1 gap-5 sm:gap-6 sm:grid-cols-2 lg:grid-cols-4"
+      >
+        {mainAccounts?.map(function (mainAccount, idx) {
+          console.log(mainAccount);
+          return (
+            <>
+              <p>{mainAccount.type_name}</p>
+              {mainAccount.accounts.accounts.map(function (account) {
+                return <p>{account.accountUid}</p>;
+              })}
+            </>
+          );
+        })}
+      </ul>
     </>
   );
 }
--- pages/settings.tsx
+++ pages/settings.tsx
@@ -1,25 +1,15 @@
 import { Layout } from "../components/Layout";
-import { useGetAccountsAccountsGet } from "../api/service/accounts";
 
-export default function Settings() {
-  const response = useGetAccountsAccountsGet();
-  const main_accounts = response.data?.main_accounts;
-  console.log(main_accounts[0].accounts.accounts);
+export default function AccountDetails() {
   return (
     <>
       {/* Main content title */}
       <h1 className={" text-2xl font-semibold pb-4"}>Settings</h1>
 
       {/* Content */}
-      <h2 className="text-gray-500 text-xs font-medium uppercase tracking-wide">
-        Accounts:
-      </h2>
-      <ul
-        role="list"
-        className="mt-3 grid grid-cols-1 gap-5 sm:gap-6 sm:grid-cols-2 lg:grid-cols-4"
-      ></ul>
+      <div></div>
     </>
   );
 }
 
-Settings.Layout = Layout;
+AccountDetails.Layout = Layout;
